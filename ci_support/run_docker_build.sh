94c515d262b40b00282627ed43b6021b86b55589
Jonathan Helmus <jjhelmus@gmail.com>
2017-06-18 09:05:02 -0500

    MNT: Re-rendered with conda-smithy 2.3.2.post.dev30

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -57,16 +57,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 3 case(s).
-    export CONDA_PY=27
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    export CONDA_PY=35
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
-
-    export CONDA_PY=36
+# Embarking on 1 case(s).
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 touch /feedstock_root/build_artefacts/conda-forge-build-done
